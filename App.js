8f008f9d5f7019e56ef963757177272d6bb4c0bb
EmmadRaheem <EmmadRaheem@users.noreply.github.com>
2020-06-06 08:53:02 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+    return(
+        <img src="logo.png" alt="Total Petrol Pump Logo" title="logo"
+        height="400" width="400"/>
+    )
+
+}
+
+export default App;
